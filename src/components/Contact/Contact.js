96787c344964b01c04852fc686b5b547724a3cf2
Austin Wright <awguitar86@gmail.com>
2018-05-16 22:32:46 -0600

    added more pictures to the gallery. Finished the Contact page, except for responsive.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import HeaderTwo from '../HeaderTwo/HeaderTwo';
+import FruitDessert from '../../images/fruit-dessert.jpg';
+import SteakAndPotatoes from '../../images/steak-potatoes.jpg';
+import AquariumLogo from '../../images/AquariumLogo-white.png';
 
 import './contact.css';
 
@@ -10,24 +13,35 @@ class Contact extends Component {
             <div className="contact-container">
                 <HeaderTwo />
                 <div className="contact-wrap">
-                    <h1>CONTACT US</h1>
-                    <p>
+                    <h1>Contact Us</h1>
+                    <p className='contact-description-address'>
                         If you would like to schedule an event at the Loveland Living Planet Aquarium
                         or have any other questions, please contact us. We are more than happy to help
                         you with your catering needs.
                     </p>
-                    <p>
-                        The Loveland Living Planet Aquarium
-                        <br/>
+                    <img src={AquariumLogo} alt='aquarium logo' className='contact-aqua-logo'/>
+                    <p className='contact-description-address'>
                         12033 South Lone Peak Parkway Draper, UT 84020
                     </p>
                     <div className='contact-info'>
-                        <div className='keith-info'></div>
-                        <div className='chelsea-info'></div>
+                        <div className='keith-info'>
+                            <p>Keith Prows PMP</p>
+                            <p>General Manager</p>
+                            <p>Office: (801) 355-3474 x449</p>
+                            <p>Mobile: (801) 633-6991</p>
+                            <p>keith@iconculinary.com</p>
+                        </div>
+                        <div className='chelsea-info'>
+                            <p>Chelsea</p>
+                            <p>Event Coordinator</p>
+                            <p>Office: (801) 444-4444 x444</p>
+                            <p>Mobile: (801) 444-4444</p>
+                            <p>chelsea@iconculinary.com</p>
+                        </div>
                     </div>
-                    <div clasName='contact-images'>
-                        <img />
-                        <img />
+                    <div className='contact-images'>
+                        <img src={SteakAndPotatoes} alt='steak and potatoes'/>
+                        <img src={FruitDessert} alt='fruite dessert'/>
                     </div>
                 </div>
             </div>
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -6,6 +6,18 @@ import blueCupcakes from './GalleryPics/blueCupcakes.jpg';
 import food1 from './GalleryPics/tableOfFood1.jpg';
 import food2 from './GalleryPics/tableOfFood2.jpg';
 import food3 from './GalleryPics/tableOfFood3.jpg';
+import cookies from './GalleryPics/cookies1.jpg';
+import event1 from './GalleryPics/eventPic1.jpg';
+import fruit1 from './GalleryPics/fruit1.jpg';
+import salad1 from './GalleryPics/saladPic1.jpg';
+import plate1 from './GalleryPics/plateOfFood1.jpg';
+import soda1 from './GalleryPics/sodaPic1.jpg';
+import event2 from './GalleryPics/eventPic2.jpg';
+import plate2 from './GalleryPics/plateOfFood2.jpg';
+import food4 from './GalleryPics/tableOfFood4.jpg';
+import fruit2 from './GalleryPics/fruit2.jpg';
+import plate3 from './GalleryPics/plateOfFood3.jpg';
+import food5 from './GalleryPics/tableOfFood5.jpg';
 
 class Gallery extends Component {
 
@@ -20,6 +32,18 @@ class Gallery extends Component {
                         <img src={food1} alt='pic1'/>
                         <img src={food2} alt='pic1'/>
                         <img src={food3} alt='pic1'/>
+                        <img src={event1} alt='pic1'/>
+                        <img src={fruit1} alt='pic1'/>
+                        <img src={cookies} alt='pic1'/>
+                        <img src={salad1} alt='pic1'/>
+                        <img src={plate1} alt='pic1'/>
+                        <img src={soda1} alt='pic1'/>
+                        <img src={event2} alt='pic1'/>
+                        <img src={plate2} alt='pic1'/>
+                        <img src={food4} alt='pic1'/>
+                        <img src={fruit2} alt='pic1'/>
+                        <img src={plate3} alt='pic1'/>
+                        <img src={food5} alt='pic1'/>
                     </div>
                 </div>
             </div>
