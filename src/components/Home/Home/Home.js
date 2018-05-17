bff9025c9b70ec8543defbe41d8ba05de52b2d91
Austin Wright <awguitar86@gmail.com>
2018-05-17 12:36:37 -0600

    got header to change when scrolling down. But it's glitchy.

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import './home.scss';
+import './home.css';
 
 import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
 import Welcome from '../Welcome/Welcome';
 import About from '../About/About';
 import Sustain from '../Sustainability/Sustain';
+import HeaderTwo from '../../HeaderTwo/HeaderTwo';
 
 export default class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            fixHeader: false
+        }
+
+    }
+
+    componentDidMount(){
+        window.addEventListener('scroll', () => {
+            console.log(window.pageYOffset);
+            if(window.pageYOffset > 100){
+                this.setState({fixHeader: true});
+            }else {
+                this.setState({fixHeader: false});
+            }
+        })
+    }
+
+
     render () {
         return(
             <div className='home-body'>
-                <Header />
+                {!this.state.fixHeader ? <Header className='home-header'/> : <HeaderTwo className='home-header-two'/>}
                 <Hero />
                 <Welcome />
                 <About />
